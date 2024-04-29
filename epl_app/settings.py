a941e93c1e25a4b70a4e5a90e53eb3b8b01fe46d
Alex Jeffries <alex@passportcreative.co>
2024-04-29 14:22:58 +0200

    email change

--- epl_app/settings.py
+++ epl_app/settings.py
@@ -131,11 +131,12 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'mail.spacemail.com'  # For Gmail
+EMAIL_HOST = 'mail.spacemail.com'  
 EMAIL_PORT = 465
 EMAIL_USE_SSL = True
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER', 'default-email@example.com')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD', 'default-password')
+DEFAULT_FROM_EMAIL = 'Alex <hi@easypackinglist.com>'
 
 LOGIN_REDIRECT_URL = '/'
 
